e3195ef73b3366c904b37c4c0d825883eaded97d
wanderjucelio733 <wanderjucelio733@gmail.com>
2025-03-08 05:16:14 +0000

    Add 3.cpp

---
+++ src/3.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    int x = 5;
+    double y = sqrt(x);
+    std::cout << "The square root of " << x << " is " << y << std::endl;
+    return 0;
+}
