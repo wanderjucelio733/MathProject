f177d38a1011184e3130c447ca599059edd761f4
wanderjucelio733 <wanderjucelio733@gmail.com>
2025-03-10 12:07:35 +0000

    Add 5.cpp

---
+++ src/5.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1 = 5;
+    int num2 = 3;
+    cout << "The sum of " << num1 << " and " << num2 << " is: " << (num1 + num2) << endl;
+    return 0;
+}
