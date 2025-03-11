70124bc31a253d5ed753e9de515f958913116d7a
wanderjucelio733 <wanderjucelio733@gmail.com>
2025-03-11 15:45:04 +0000

    Add 6.cpp

---
+++ src/6.cpp
@@ -0,0 +1,9 @@
+  #include <cmath>
+  
+  int main() {
+    double x = 2;
+    double y = 3;
+    double z = sqrt(x*x + y*y);
+    cout << "The length of the vector is: " << z << endl;
+    return 0;
+  }
