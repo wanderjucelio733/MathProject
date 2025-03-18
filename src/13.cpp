85e6a424731cfd6479723eb7fdd4d6c6c4185cc8
wanderjucelio733 <wanderjucelio733@gmail.com>
2025-03-18 10:20:24 +0000

    Add 13.cpp

---
+++ src/13.cpp
@@ -0,0 +1,29 @@
+int main(){
+    double a = 3;
+    double b = 2;
+    double c = 5;
+    double d = 8;
+    double e = 7;
+    double f = 10;
+    double g = 9;
+    double h = 12;
+    double i = 4;
+    double j = 6;
+    double k = 15;
+    double l = 8;
+    double m = 7;
+    double n = 13;
+    double o = 10;
+    double p = 6;
+    double q = 12;
+    double r = 9;
+    double s = 15;
+    double t = 8;
+    double u = 7;
+    double v = 13;
+    double w = 10;
+    double x = 6;
+    double y = 12;
+    double z = 9;
+    return 0;
+}
