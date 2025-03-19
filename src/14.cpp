4f34cca5401962cb78be9ddf556465d6a2525bbb
wanderjucelio733 <wanderjucelio733@gmail.com>
2025-03-19 12:25:18 +0000

    Add 14.cpp

---
+++ src/14.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+  int x = 5;
+  int y = 3;
+  cout << "The sum of " << x << " and " << y << " is: " << x + y << endl;
+  return 0;
+}
