1fafb8e87e355c24f5925bafe711be06523a009a
wanderjucelio733 <wanderjucelio733@gmail.com>
2025-03-23 20:13:31 +0000

    Add 17.cpp

---
+++ src/17.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    int y = 10;
+    int result = x + y;
+    cout << "The sum of " << x << " and " << y << " is: " << result << endl;
+    return 0;
+}
