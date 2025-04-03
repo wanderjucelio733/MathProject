ea6a1d7e87718d55191e999545296df375212f6a
wanderjucelio733 <wanderjucelio733@gmail.com>
2025-04-03 12:58:00 +0000

    Add 24.cpp

---
+++ src/24.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int num1 = 5;
+    int num2 = 3;
+
+    std::cout << "The sum of " << num1 << " and " << num2 << " is: " << (num1 + num2) << std::endl;
+
+    return 0;
+}
