5351bc4867e331b525e5b03cbc5809764718ff3e
wanderjucelio733 <wanderjucelio733@gmail.com>
2025-04-05 06:05:01 +0000

    Add 25.cpp

---
+++ src/25.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 10;
+    
+    cout << "The sum of " << a << " and " << b << " is: " << a + b << endl;
+    
+    return 0;
+}
