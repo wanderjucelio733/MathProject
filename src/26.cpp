67ee050311f65ae9c0d0be85f2c0239d38ebbded
wanderjucelio733 <wanderjucelio733@gmail.com>
2025-04-07 03:00:44 +0000

    Add 26.cpp

---
+++ src/26.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    int y = 10;
+    float z = 3.5f;
+
+    cout << "The sum is: " << x + y << endl;
+    cout << "The product of z and y: " << z * y << endl;
+    return 0;
+}
