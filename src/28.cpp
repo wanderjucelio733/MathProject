8c92a5b946efffed25b8c159df1a7284929cd359
wanderjucelio733 <wanderjucelio733@gmail.com>
2025-04-11 11:32:45 +0000

    Add 28.cpp

---
+++ src/28.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1 = 5;
+    int num2 = 3;
+    int result = num1 + num2;
+    cout << "The sum of " << num1 << " and " << num2 << " is: " << result << endl;
+    return 0;
+}
