f21a255f5c7d09a8258a723201352a38738fc622
wanderjucelio733 <wanderjucelio733@gmail.com>
2025-05-23 08:08:22 +0000

    Add 37.cpp

---
+++ src/37.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    int y = 10;
+    int sum = x + y;
+    cout << "The sum of " << x << " and " << y << " is: " << sum << endl;
+    return 0;
+}
