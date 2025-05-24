8482bdbfb80182c895e7c0f4a34ebb441d923065
wanderjucelio733 <wanderjucelio733@gmail.com>
2025-05-24 05:09:47 +0000

    Add 38.cpp

---
+++ src/38.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    if (x > 10) {
+        cout << "The number is greater than 10." << endl;
+    } else {
+        cout << "The number is not greater than 10." << endl;
+    }
+    return 0;
+}
