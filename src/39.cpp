5a243337fd6f9ee7639169bc026cac07a077d7b6
wanderjucelio733 <wanderjucelio733@gmail.com>
2025-05-25 21:55:51 +0000

    Add 39.cpp

---
+++ src/39.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x = 5;
+    if (x > 0) {
+        cout << "x is positive" << endl;
+    } else {
+        cout << "x is not positive" << endl;
+    }
+    return 0;
+}
